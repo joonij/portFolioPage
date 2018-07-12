46857aeafb2d5cce350d1025ed1a6c82b5a8c849
joonij <37970610+joonij@users.noreply.github.com>
2018-07-12 17:25:06 +0900

    release 20180712

--- js/javascript.js
+++ js/javascript.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $("#row_1").fadeIn(2000, function(){
         $(".content_row").fadeIn(1000);
     });
+    // Click Plus to Minus
+	$( '.plus-to-minus' ).click( function( event ) {
+		$( this ).toggleClass( 'minus' );
+        $(".project").addClass("");
+	});
 });
