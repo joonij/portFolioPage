9ff4367bf2b0043768b2065ce73c981331e5582a
joonij <37970610+joonij@users.noreply.github.com>
2018-07-13 01:14:25 +0900

    release 20180713

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-    $("#row_1").fadeIn(2000, function(){
-        $(".content_row").fadeIn(1000);
+    // Start
+    $(".content_row").fadeIn(2000);
+    //Scroll Down
+    $(window).scroll(function(){
+        var scrollValue = $(document).scrollTop();
+        (scrollValue >= 400) ? $("#row_1 span").fadeOut("1000") : $("#row_1 span").fadeIn("1000");
     });
     // Click Plus to Minus
 	$( '.plus-to-minus' ).click( function( event ) {
-		$( this ).toggleClass( 'minus' );
-        $(".project").addClass("");
+		$(this).toggleClass( 'minus' );
+        ($(this).parent().parent().css("height") === "170px") ? $(this).parent().parent().css("height","700px") : $(this).parent().parent().css("height","170px");
 	});
 });
