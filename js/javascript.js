95442475c314062cd9f567e8c3ed9de70a2816fe
joonij <37970610+joonij@users.noreply.github.com>
2018-07-13 17:44:38 +0900

    release 20180713

--- js/javascript.js
+++ js/javascript.js
@@ -7,8 +7,14 @@ $(document).ready(function(){
         (scrollValue >= 400) ? $("#row_1 span").fadeOut("1000") : $("#row_1 span").fadeIn("1000");
     });
     // Click Plus to Minus
-	$( '.plus-to-minus' ).click( function( event ) {
-		$(this).toggleClass( 'minus' );
-        ($(this).parent().parent().css("height") === "170px") ? $(this).parent().parent().css("height","700px") : $(this).parent().parent().css("height","170px");
+	$( '.plus-to-minus' ).click(function(event){
+        $(this).toggleClass( 'minus' );
+        if($(this).parent().css("height") === "170px") {
+            $(this).parent().css("height","auto");
+            $(this).siblings(".detail").fadeIn(500);
+        } else {
+            $(this).parent().css("height","170px");
+            $(this).siblings(".detail").fadeOut(100);
+        }
 	});
 });
